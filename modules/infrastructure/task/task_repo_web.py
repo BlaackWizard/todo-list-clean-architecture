6e00c01c87836d9ce219c978f36f4c5fe59bc190
BlaackWizard <kabynks@gmail.com>
2024-11-11 09:30:48 +0500

    [ADD] realization into task app

--- modules/infrastructure/task/task_repo_web.py
+++ modules/infrastructure/task/task_repo_web.py
@@ -1,2 +1,30 @@
-from modules.domain.repositories.task_repo import TaskRepo
+from typing import List
 
+from modules.domain.repositories.task_repo import TaskRepo
+from .models import TaskModel
+from ...entities.Task import TaskEntity
+
+
+class TaskRepoWeb(TaskRepo):
+    def get_task(self, task_id: int):
+        task = TaskModel.object.get(id=task_id)
+        return task
+
+    def list_tasks(self):
+        tasks = TaskModel.objects.all()
+        return tasks
+
+    def save_task(self, task: TaskEntity):
+        task_model = self._to_model(task)
+        task_model.save()
+
+    def delete_task(self, task: TaskEntity):
+        task_model = self._to_model(task)
+        task_model.delete()
+    def _to_model(self, task: TaskEntity):
+        return TaskModel(
+            id=task.id,
+            title=task.title,
+            description=task.description,
+            confirmed=task.confirmed,
+        )
