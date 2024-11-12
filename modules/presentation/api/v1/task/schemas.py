c51710fc95b8091c32a1f8647a3f77ca4f9f69a2
BlaackWizard <kabynks@gmail.com>
2024-11-12 09:51:25 +0500

    [ADD] task api

---
+++ modules/presentation/api/v1/task/schemas.py
@@ -0,0 +1,12 @@
+from ninja import Schema
+
+class TaskInSchema(Schema):
+    id: int
+    title: str
+    description: str
+
+class TaskOutSchema(Schema):
+    id: int
+    title: str
+    description: str
+    confirmed: bool = False
